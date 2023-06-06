26c8fda330b1daa3203f63ea58a3676c0389bd0c
shaharsarfaty <sarfaty@mail.tau.ac.il>
2023-06-06 20:02:39 +0300

    Modifications for our settings

--- examples/imagic/launch.sh
+++ examples/imagic/launch.sh
@@ -1,8 +1,8 @@
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-export OUTPUT_DIR="../../../models/imagic"
-export INPUT_IMAGE="imgs/Official_portrait_of_Barack_Obama.jpg"
+export OUTPUT_DIR="/home/dcor/sarfaty/stable_retribution/imagic"
+export INPUT_IMAGE="/home/dcor/sarfaty/git/diffusers_ShivamShrirao/examples/imagic/imgs/Official_portrait_of_Barack_Obama.jpg"
 
-accelerate launch train_imagic.py \
+/home/dcor/adihaviv1/miniconda3/envs/unitune2/bin/accelerate launch /home/dcor/sarfaty/git/diffusers_ShivamShrirao/examples/imagic/train_imagic.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --output_dir=$OUTPUT_DIR \
   --input_image=$INPUT_IMAGE \
@@ -15,4 +15,5 @@ accelerate launch train_imagic.py \
   --emb_learning_rate=1e-3 \
   --learning_rate=1e-6 \
   --emb_train_steps=500 \
-  --max_train_steps=1000
+  --max_train_steps=1000 \
+  --gradient_checkpointing
